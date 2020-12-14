12e2e8c0530b7f9fa6811589e8c400c8cb9cbbb1
Ilhee <asahi8769@gmail.com>
2020-12-14 15:53:43 +0900

    20201214_15

--- experiment/main.py
+++ experiment/main.py
@@ -12,6 +12,7 @@ train_iter, test_iter = data.get_iter(batch_sizes=(32, 256))
 
 # model = RNNModel(n_vocab=3000, n_input=20, n_hidden=15, n_rnnlayers=1, n_outputs=512)
 model = CNNModel(n_vocab=3000, n_embedding=128, n_outputs=512)
+model.load_checkpoint()
 trainer = ModelTrainer(model, train_iter, test_iter, lr=0.00001, epochs=1000)
 trainer.batch_learn()
 trainer.plot_result()
