5b4eb7e4c8f894e0b4c3f8afa85f73165812a758
Ilhee <asahi8769@gmail.com>
2020-12-30 10:22:09 +0900

    20201230_10

---
+++ experiment/dataset.py
@@ -0,0 +1,154 @@
+from searches.problemsys_search import *
+from searches.filters import prob_words_not_to_skip, prob_additional_exceptions
+from searches.partsys_search import partsys_3_search
+from utils.functions import show_elapsed_time
+import torchtext.data as ttd
+import torch
+from experiment.config import *
+
+
+class Dataset:
+    def __init__(self, file_path, split_ratio=split_ratio, update_csv=False):
+        self.split_ratio = split_ratio
+        self.tar_tokens = None
+        self.document = None
+        self.encoder = dict()
+        self.decoder = dict()
+        self.tar_encoder = dict()
+        self.tar_decoder = dict()
+        self.file_path = file_path
+        self.spawn_path = 'files\spawn\experiment_data.csv'
+        self.train_dataset = None
+        self.test_dataset = None
+        self.text = None
+        self.label = None
+        self.dataset = None
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.df = self.get_dataframe()
+        self.save_target_encoder()
+        self.save_target_decoder()
+        if update_csv:  # csv파일과 타깃 인코더를 업그레이드할때만 사용함
+            self.df = partsys(self.df)
+            self.df = preprocess(self.df)
+            self.encode_target()
+            self.spawn()
+        else:
+            self.encode_dataset()
+
+
+    @show_elapsed_time
+    def get_dataframe(self):
+        with open(self.file_path, 'rb') as file:
+            # df = pd.read_excel(file, usecols="F, H, K, L")
+            df = pd.read_excel(file)
+            df.rename(columns={'품번': 'Part No', '품명': '부품명'}, inplace=True)
+            print(df)
+        return df
+
+    @show_elapsed_time
+    def spawn(self):
+        self.df[['data', 'encoded_target']].to_csv(self.spawn_path, index=False, encoding='utf-8-sig')
+        os.startfile(self.spawn_path)
+
+    @show_elapsed_time
+    def save_encoder_decoder(self):
+        with open('files/spawn/encoder.pkl', 'wb') as f:
+            pickle.dump(self.text.vocab.stoi, f)
+        with open('files/spawn/decoder.pkl', 'wb') as f:
+            pickle.dump(self.text.vocab.itos, f)
+        # print(self.text.vocab.stoi)
+
+    @show_elapsed_time
+    def save_target_encoder(self):
+        idx = 0
+        self.tar_tokens = self.df['target'].tolist()
+        for word in self.tar_tokens:
+            if word not in self.tar_encoder:
+                self.tar_encoder[word] = idx
+                idx += 1
+        with open('files/spawn/tar_encoder.pkl', 'wb') as f:
+            pickle.dump(self.tar_encoder, f)
+        print("Target :",len(self.tar_encoder.keys()),"classes")
+
+    @show_elapsed_time
+    def save_target_decoder(self):
+        self.tar_decoder = dict(zip(self.tar_encoder.values(), self.tar_encoder.keys()))
+        with open('files/spawn/tar_decoder.pkl', 'wb') as f:
+            pickle.dump(self.tar_decoder, f)
+        # print("decoder part")
+        # print(self.tar_decoder)
+
+    @show_elapsed_time
+    def encode_target(self):
+        self.df['encoded_target'] = [self.tar_encoder.get(i, 0) for i in self.df['target']]
+
+    @show_elapsed_time
+    def encode_dataset(self):
+        self.text = ttd.Field(sequential=True, batch_first=True, lower=False,
+                              tokenize=str.split,
+                              pad_first=True, fix_length=fix_length)
+        self.label = ttd.Field(sequential=False, use_vocab=False, is_target=True)
+        self.dataset = ttd.TabularDataset(path=self.spawn_path, format='csv', skip_header=True,
+                                          fields=[('data', self.text), ('encoded_target', self.label)])
+        self.train_dataset, self.test_dataset = self.dataset.split(split_ratio=self.split_ratio)
+
+        self.text.build_vocab(self.dataset, min_freq=3, max_size=n_vocab)
+        self.save_encoder_decoder()
+
+    def get_iter(self, batch_sizes=(32, 256)):
+        train_iter, test_iter = ttd.Iterator.splits((self.train_dataset, self.test_dataset), sort_key=lambda x: len(x.data),
+                                                    batch_sizes=batch_sizes, device=self.device)
+        return train_iter, test_iter
+
+
+def preprocess(df, for_train=True):
+    part_list, supplier_list = get_basic_filters()
+    words_to_skip = [i for i in part_list + supplier_list if i not in prob_words_not_to_skip]
+    words_to_skip += prob_additional_exceptions
+    print("Filtering :", len(words_to_skip))
+    word_list = []
+    title = [i.upper() for i in df['제목'].tolist()]
+    for i, n in enumerate(title):
+        name = n.replace("SUB-PART", "SUBPART").replace("SUB PART", "SUBPART").replace(
+            "SUB PART PROBLEM", "SUBPART").replace("PARTS", "PART").replace("으로", "").replace(
+            "PRESSURE", "압력").replace("NOT FIXED", "SUBPART").replace("FITTING", "FIT").replace(
+            "FITTED","FIT").replace("NOT FIT", "UNFITTING").replace("BAD FIT", "UNFITTING").replace(
+            "갭", "GAP").replace("WELDING/PRESS", "웰딩/프레스").replace(
+            "WELD LINE", "WELDLINE").replace("홀", "HOLE").replace("BAR CODE", "BARCODE")
+        name = re.sub("(NO)(\.)[0-9\s]+|[0-9][A-Z]{2}($|[\s,.\-_)])|[0-9_]|[\W]", ' ', name)
+        words = [i for i in name.split(' ') if i not in words_to_skip and len(i) > 1]
+        word_list.append(words)
+    df['정리'] = [' '.join(i) for i in word_list]
+    df['data'] = df['정리'] + ' ' + df['부품체계_1'] + ' ' + df['부품체계_2'] + ' ' + df['부품체계_3']
+    df['data'] = [re.sub("[,.\-_&]", "", str(i)).replace("  ", " ") for i in df['data'].tolist()]
+    df['data_len'] = [len(i.split(' ')) for i in df['data'].tolist()]
+    if for_train:
+        df['target'] = [str(i).replace(' ', '') for i in df['target'].tolist()]
+        df['target_빈도'] = [Counter(df['target'].tolist())[i] for i in df['target'].tolist()]
+        df_ = df[['부품명', 'Part No', '제목', '정리', '부품체계_1', '부품체계_2', '부품체계_3', 'data', 'target',
+                           'target_빈도', 'data_len']]
+        return df_
+    else:
+        return df
+
+
+@show_elapsed_time
+def partsys(df):
+    with open('files/품목구분기준.xlsx', 'rb') as file:
+        df_1 = pd.read_excel(file, sheet_name='부품체계1')
+        df_2 = pd.read_excel(file, sheet_name='부품체계2')
+    partsys_1_dict = dict(zip([str(i) for i in df_1['품번'].tolist()], df_1['부품구분']))
+    partsys_2_dict = {str(p): df_2['부품구분'].tolist()[n] for n, p in enumerate(df_2['품번'].tolist())}
+    df['부품체계_1'] = [partsys_1_dict.get(i[0], '') for i in df['Part No'].tolist()]
+    df['부품체계_2'] = [partsys_2_dict.get(i[:2], '') for i in df['Part No'].tolist()]
+    df = partsys_3_search(df)
+    return df
+
+
+if __name__ == "__main__":
+    os.chdir(os.pardir)
+    data = Dataset(file_path=file_path, update_csv=True)
+    # data = Dataset(file_path='files/불량유형수기정리.xlsx', update_csv=False)
+    # data.encode_data()
+    # print(data.tokens)
+
---
+++ searches/problemsys_test.py
@@ -0,0 +1,108 @@
+from searches.problemsys_search import *
+from master_db import MasterDBStorage
+
+
+def problem_data():
+    with open('files/불량유형수기정리.xlsx', 'rb') as file:
+        df = pd.read_excel(file, usecols="B:C, F, H, K, L, O")
+        df.sort_values("불량구분", inplace=True)
+        df.rename(columns={'불량구분.1': '대분류', '품번': 'Part No', '품명': '부품명'}, inplace=True)
+        df.drop_duplicates(subset="제목", keep='first', inplace=True)
+        df['발생빈도'] = [Counter(df['불량구분'].tolist())[i] for i in df['불량구분'].tolist()]
+        df.sort_values("발생빈도", inplace=True, ascending=False)
+    return df
+
+
+def partsys_1_2(df):
+    with open('files/품목구분기준.xlsx', 'rb') as file:
+        df_1 = pd.read_excel(file, sheet_name='부품체계1')
+        df_2 = pd.read_excel(file, sheet_name='부품체계2')
+    partsys_1_dict = dict(zip([str(i) for i in df_1['품번'].tolist()], df_1['부품구분']))
+    partsys_2_dict = {str(p): df_2['부품구분'].tolist()[n] for n, p in enumerate(df_2['품번'].tolist())}
+    df['부품체계_1'] = [partsys_1_dict.get(i[0], '') for i in df['Part No'].tolist()]
+    df['부품체계_2'] = [partsys_2_dict.get(i[:2], '') for i in df['Part No'].tolist()]
+    return df
+
+
+def exp_data():
+    df = MasterDBStorage('해외불량이력', append_from_file=True).df
+    df['품명'] = [i.upper() for i in df['품명'].tolist()]
+    df.rename(columns={'품번': 'Part No', '품명': '부품명'}, inplace=True)
+    df.drop_duplicates(subset="Part No", keep='first', inplace=True)
+    df.drop_duplicates(subset="부품명", keep='first', inplace=True)
+    return df
+
+
+def dom_data():
+    df = MasterDBStorage('입고불량이력',append_from_file=True).df
+    df['부품명'] = [i.upper() for i in df['부품명'].tolist()]
+    return df
+
+
+def problem_type_dict():
+    with open('files/불량구분기준.xlsx', 'rb') as file:
+        df = pd.read_excel(file, sheet_name='불량구분_1')
+        df.fillna('', inplace=True)
+        df['불량길이'] = [len(df['제목_1'].tolist()[n].split(', ')) for n in range(len(df))]
+        df.sort_values(["불량길이"], inplace=True, ascending=[False])
+        key_sequence = df['제목_1'].tolist()
+    classifier_dict = {i: {'불량구분': df['불량구분'].tolist()[n]} for n, i in enumerate(key_sequence)}
+    key_sequence = remove_duplication(key_sequence)
+    return classifier_dict, key_sequence
+
+
+if __name__ == "__main__":
+    os.chdir(os.pardir)
+    print("Current working directory: {0}".format(os.getcwd()))
+
+    # df = problem_data()
+    df = dom_data()
+    df = partsys_1_2(df)
+    classifier_dict, key_sequence = problem_type_dict()
+    df = problemsys_search(df, classifier_dict, key_sequence)
+
+    problem_list = remove_duplication(df['불량구분'].tolist())
+    print(problem_list[40:60])
+    print(sum(list(Counter(df['불량구분'].tolist()).values())[0:40]) / len(df))
+
+    df = df[['불량구분', '제목', '부품체계_2', '제목_1', '불량정리', '불량구분결과']]
+    df.drop_duplicates(subset="제목_1", keep='first', inplace=True)
+
+    filename = "불량구분결과_test"
+    with pd.ExcelWriter(rf'files\spawn\{filename}.xlsx') as writer:
+        df.to_excel(writer, sheet_name='품번체계', index=False)
+    os.startfile(rf'files\spawn\{filename}.xlsx')
+
+
+    # ['스크래치', '파손', '오염', '작동불량', '노이즈', '이종', '기포', '크랙', '이물유입', '갭', '서브파트누락', 'DTC불량',
+    # '도장이물', '누유', '휨', '칩', '치수불량', '미성형', '수량부족', '찍힘']
+    # ['덴트', '패드이탈', '버', '발청', '홀누락', '깨짐', '기공', '누수', '홀위치불량', '핀휨', '서브파트이종', '눌림', '플레이트오조립', '웰드너트위치불량', '바디갭',
+    #  '핀홀', '홀치수불량', '주름', '도장기포', '라벨누락']
+    # ['이색', '굴곡', '가스켓이탈', '찢어짐', '볼트누락', '사운드작동불가', '커버닫힘불량', '서브파트오조립', '벌브소손', '들뜸', '브라켓누락', '렌즈조정불량', '너트누락',
+    #  '패드짧음', '잠김동작불량', '콜드테스트불량', '점등불량', '볼트덜조임', '와셔이탈', '작동속도느림']
+
+    # filter = '이색'
+    # filter = None
+
+    # for n, i in enumerate(problem_list):
+    #     try:
+    #         df_partial = df[df['불량구분'] == i]
+    #     except KeyError:
+    #         print('passing', i)
+    #     else:
+    #         if filter is not None and i == filter:
+    #             print(i, remove_duplication(df_partial['제목_1'].tolist()))
+    #             print(sorted(flatten([i.split(', ') for i in remove_duplication(df_partial['제목_1'].tolist())])))
+    #             df = df[df['불량구분'] == filter]
+
+    # df.drop_duplicates(subset="제목_1", keep='first', inplace=True)
+    # df.drop_duplicates(subset=['제목_1', '부품체계_2'], keep='first', inplace=True)
+    #
+    # df = df[['제목', '불량구분', '부품체계_2', '제목_1', '제목_1_발생빈도']]
+    # df['불량길이'] = [len(df['제목_1'].tolist()[n].split(', ')) for n in range(len(df))]
+    # df.sort_values(["불량길이", "제목_1_발생빈도"], inplace=True, ascending=[False, False])
+    #
+    # filename = "불량구분결과_test"
+    # with pd.ExcelWriter(rf'files\spawn\{filename}.xlsx') as writer:
+    #     df.to_excel(writer, sheet_name='품번체계', index=False)
+    # os.startfile(rf'files\spawn\{filename}.xlsx')
