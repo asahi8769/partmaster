061805b214f8fd4e28d733d770276bc617056c42
Ilhee <asahi8769@gmail.com>
2021-01-06 15:03:30 +0900

    20210106_15

---
+++ experiment/config.py
@@ -0,0 +1,12 @@
+file_path = 'files/불량유형수기정리_v2.xlsx'
+
+n_vocab = 800
+n_embedding = 128
+n_outputs = 81
+
+split_ratio = 0.8
+batch_sizes = (16, 256)
+fix_length = 15
+
+lr = 0.00001
+epochs = 600
